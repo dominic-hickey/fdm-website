8243d40b6d8aeacfc99b2e84693d29059e4dc048
Dominic Hickey <dominic.j.hickey@gmail.com>
2016-01-21 16:33:11 +0000

    Restructured CSS

--- js/navigation.js
+++ js/navigation.js
@@ -1,14 +1,17 @@
+// -------------------------------------
+// Kings Template: JS > Navigation
+// -------------------------------------
+
+
 // Toggles class open to display/hide the whole navigation
 $( ".widget__extendednavigation" ).addClass( "closed" );
 
 $( ".extendednavigation__navigation-toggle" ).click(function() {
 	$( ".widget__extendednavigation" ).toggleClass( "open" ).toggleClass( "closed" );
-
-    $( ".page-content" ).toggleClass( "navigation--open" );
 });
 
-// Togglse class open to display/hide folders
 
+// Togglse class open to display/hide folders
 $( ".navigation-item.folder .item-name--parent" ).each(function() {
 	$( this ).click(function() {
 			$( this ).parent( ".navigation-item.folder" ).toggleClass( "open" );
